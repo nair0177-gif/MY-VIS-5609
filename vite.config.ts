9d4177997c9445ba150e96c00884a1a547e5f8b0
AnaRian7 <anarian7203@gmail.com>
2026-09-18 23:05:44 -0500

    Fix SvelteKit adapter configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
-		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries.
-				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			}
-		})
+		sveltekit()
 	]
 });
